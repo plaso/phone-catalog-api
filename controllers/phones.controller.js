a75dd4350629ac048cabc9496872ffe325bcc629
Pablo Laso <plasocortabitarte@gmail.com>
2018-05-19 20:12:00 +0200

    create method and api error handler

--- controllers/phones.controller.js
+++ controllers/phones.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Phone = require('../models/phone.model');
+const ApiError = require('../models/api-error.model');
 
 module.exports.list = (req, res, next) => {
   Phone.find()
@@ -7,4 +8,19 @@ module.exports.list = (req, res, next) => {
       res.json(phones);
     })
     .catch(error => next(error));
+}
+
+module.exports.create = (req, res, next) => {
+  const phone = new Phone(req.body);
+  phone.save()
+    .then(() => {
+      res.status(201).json(phone);
+    })
+    .catch(error => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        next(new ApiError(error.errors));
+      } else {
+        next(new ApiError(error.message, 500));
+      }
+    })
 }
